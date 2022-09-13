a39765aa7bf78269509034c7d83dfb58fab0c7f6
doo <gostbaducking1@nate.com>
2022-09-13 21:39:19 +0900

    feat: project first commit

---
+++ src/main/java/com/doo/comeinwebflux/ComeinWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.doo.comeinwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComeinWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComeinWebfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doo/comeinwebflux/ComeinWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doo.comeinwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComeinWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
